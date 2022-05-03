a9873e380476d61b09481be11b2e44d0be3bd2a4
Stas Ukolov <ukoloff@gmail.com>
2022-05-03 09:08:27 +0500

    Use env vars

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,9 +9,13 @@ from .models import Greeting
 def index(request):
     # return HttpResponse('Hello from Python!')
     # return render(request, "index.html")
-    r = requests.get('https://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+
+    # r = requests.get('https://httpbin.org/status/418')
+    # print(r.text)
+    # return HttpResponse('<pre>' + r.text + '</pre>')
+
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 def db(request):
 
