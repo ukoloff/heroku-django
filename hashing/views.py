4a471f5d9893f0007f9b78209307de17cc3db9cf
Stas Ukolov <ukoloff@gmail.com>
2022-05-24 13:03:52 +0500

    One more test

--- hashing/views.py
+++ hashing/views.py
@@ -6,10 +6,12 @@ from . import codes
 
 # Create your views here.
 
+
 def index(req):
-  q = req.GET.get('q', '')
-  h = codes.myHash(q)
-  rsp = codes.response(h)
-  ok = codes.checkResponse(h, rsp)
-  return render(req, 'hashing/index.html',
-    {'q': q, 'h': h, 'r': rsp, 'ok': ok})
+    q = req.GET.get('q', '')
+    h = codes.myHash(q)
+    rsp = codes.response(h)
+    ok = codes.checkResponse(h, rsp)
+    nok = codes.checkResponse(h, rsp + 1)
+    return render(req, 'hashing/index.html',
+                  {'q': q, 'h': h, 'r': rsp, 'ok': ok, 'nok': nok})
