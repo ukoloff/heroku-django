c720d5b955179a41d6d9210cb1955031b6d0f59a
Stas Ukolov <ukoloff@gmail.com>
2022-05-24 12:27:58 +0500

    Show form

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,10 +1,12 @@
+import imp
 from django.urls import path, include
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
+# import hello.views
+import hashing.views
 
 # To add a new path, first import the app:
 # import blog
@@ -15,7 +17,8 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    path("", hashing.views.index, name="index"),
+    # path("", hello.views.index, name="index"),
+    # path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
--- hashing/views.py
+++ hashing/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from requests import request
 
 # Create your views here.
+
+def index(req):
+  return render(req, 'hashing/index.html', {'q': req.GET.get('q', '')})
